e6cd0ecaffc321cfb25988adaec80e2016e0efbe
stigkristiansen <stigkristiansen@outlook.com>
2016-04-29 09:43:12 +0200

    .

--- User/module.php
+++ User/module.php
@@ -21,7 +21,23 @@ class GeofenceUser extends IPSModule {
     }
 
 	public function GetURLs() {
-		$message = "This is a test\nThis is line two...";
+		$parent = IPS_GetParent($this->InstanceID);
+		$allParents = IPS_GetInstanceListByModuleID("{C5271BF2-DDC9-4EA7-8467-A8C645500263}");
+		
+		$size=sizeof($allParents);
+		$foundParent = false;
+		for($x=0;$x<$allParents;$x++) {
+			if($allParents[$x]==$parent) {
+				$foundParent = true;
+				break;
+			}
+		}
+		
+		if($foundParent) {
+			$message = "Arrival: http(s)://<server>/hook/geofence".$parent."?action=arrival&user=".$this->InstanceID;	
+		} else
+			$message = "This user do not has a Geofence Controller as parent!";
+		
 		return $message;
 	}
 
