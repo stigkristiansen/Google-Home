b76c6e20952efca5b1a93cf91f0c5ea230198649
stigkristiansen <stigkristiansen@users.noreply.github.com>
2017-01-03 19:42:09 +0100

    Update module.php

--- Controller/module.php
+++ Controller/module.php
@@ -2,37 +2,28 @@
 
 require_once(__DIR__ . "/../logging.php");
 
-class GeofenceController extends IPSModule {
+class GoogleHomeController extends IPSModule {
     
-    public function Create(){
+   public function Create(){
         parent::Create();
         
         $this->RegisterPropertyBoolean ("Log", false );
 
-		$this->RegisterPropertyString("Username", "");
-		$this->RegisterPropertyString("Password", "");
-		$this->RegisterPropertyInteger("ArrivalScript1", 0);
-		$this->RegisterPropertyInteger("ArrivalScript2", 0);
-		$this->RegisterPropertyInteger("DepartureScript1", 0);
-		$this->RegisterPropertyInteger("DepartureScript2", 0);
-		$this->RegisterPropertyBoolean("ArrivalScript1Update", false);
-		$this->RegisterPropertyBoolean("ArrivalScript2Update", false);
-		$this->RegisterPropertyBoolean("DepartureScript1Update", false);
-		$this->RegisterPropertyBoolean("DepartureScript2Update", false);
-	}
+	$this->RegisterPropertyString("Username", "");
+	$this->RegisterPropertyString("Password", "");
+
+   }
 
     public function ApplyChanges(){
         parent::ApplyChanges();
 		
-		$ident="geofence".$this->InstanceID;
-		$name="Geofence".$this->InstanceID."Hook";
+		$ident="googlehome".$this->InstanceID;
+		$name="GoogleHome".$this->InstanceID."Hook";
 		$id = $this->RegisterScript($ident, $name, "<?\n//Do not modify!\nrequire_once(IPS_GetKernelDirEx().\"scripts/__ipsmodule.inc.php\");\nrequire_once(\"../modules/Geofence/Controller/module.php\");\n(new GeofenceController(".$this->InstanceID."))->HandleWebData();\n?>");
 		$this->RegisterWebHook("/hook/".$ident, $id);
 		
-		$this->CreateVariable($this->InstanceID, "Presence", "Presence", 0, "~Presence");
-		$this->EnableAction("Presence");
 		
-		//$this->CreateVariable($this->InstanceID, "Delay", "Delay", 1, "");
+		
     }
 
     public function HandleWebData() {
@@ -52,7 +43,7 @@ class GeofenceController extends IPSModule {
 				$_SERVER['PHP_AUTH_PW'] = "";
 
 			if(($_SERVER['PHP_AUTH_USER'] != $username) || ($_SERVER['PHP_AUTH_PW'] != $password)) {
-				header('WWW-Authenticate: Basic Realm="Geofence"');
+				header('WWW-Authenticate: Basic Realm="IP-Symcon"');
 				header('HTTP/1.0 401 Unauthorized');
 				echo "Authorization required to access Symcon and Geofence";
 				$log->LogMessage("Authentication needed or invalid username/password!");
@@ -72,192 +63,12 @@ class GeofenceController extends IPSModule {
 		
 		$log->LogMessage("The controller is locked");
 		
-		$cmd="";
-		$userId="";
 		
-		if (array_key_exists('cmd', $_GET))
-			$cmd=strtolower($_GET['cmd']);
 				
-		if (array_key_exists('uid', $_GET))
-			$userId=strtolower($_GET['uid']);
-		
-		if($cmd!="" && $userId!="") {
-			$log->LogMessage("Received the command \"".$cmd."\" for user \"".IPS_GetName($userId)."\"");
-			
-			$children = IPS_GetChildrenIDs($this->InstanceID);
-			
-			$size = sizeof($children);
-			$userExists = false;
-			for($x=0;$x<$size;$x++) {
-				if($children[$x]==$userId) {
-					$userExists=true;
-					break;
-				}
-			}
-			
-			if($userExists) {
-				switch($cmd) {
-					case "arrival1":
-						$presence = true;
-						$scriptProperty = "ArrivalScript1";
-						break;
-					case "arrival2":
-						$presence = true;
-						$scriptProperty = "ArrivalScript2";
-						break;
-					case "departure1":
-						$presence = false;
-						$scriptProperty = "DepartureScript1";
-						break;
-					case "departure2":
-						$presence = false;
-						$scriptProperty = "DepartureScript2";
-						break;
-					default:
-						$log->LogMessage("Invalid command!");
-						$this->Unlock("HandleWebData");
-						return;
-				}
-				
-				$presenceId = $this->CreateVariable($userId, "Presence", "Presence", 0, "~Presence");
-				$commonPresenceId = $this->CreateVariable($this->InstanceID, "Presence", "Presence", 0, "~Presence");
-				$lastCommonPresence=GetValue($commonPresenceId);
-				$updatePresence = $this->ReadPropertyBoolean($scriptProperty."Update");
-				
-				if($updatePresence) {
-					$log->LogMessage("Updated Presence for user ".IPS_GetName($userId)." to \"".$this->GetProfileValueName(IPS_GetVariable($presenceId)['VariableCustomProfile'], $presence)."\"");
-					SetValue($presenceId, $presence);
-				}
-				
-				$commonPresence = false;
-				$users=IPS_GetInstanceListByModuleID("{C4A1F68D-A34E-4A3A-A5EC-DCBC73532E2C}");
-				$size=sizeof($users);
-				for($x=0;$x<$size;$x++){
-					if(IPS_GetParent($users[$x])==$this->InstanceID) {
-						$presenceId=$this->CreateVariable($users[$x], "Presence", "Presence", 0, "~Presence");
-						//$log->LogMessage("Checking ".IPS_GetName($users[$x]));
-						if(GetValue($presenceId)) {
-							$commonPresence = true;
-							break;
-						}
-					}
-				}
-				
-				if($updatePresence) {
-					SetValue($commonPresenceId, $commonPresence);
-					$log->LogMessage("Updated Common Presence to \"".$this->GetProfileValueName(IPS_GetVariable($commonPresenceId)['VariableCustomProfile'], $commonPresence)."\"");
-				} else
-					$log->LogMessage("Presence update is not enabled for this command.");
-				
-				$scriptId = $this->ReadPropertyInteger($scriptProperty);
-				if($scriptId>0) { 
-					$log->LogMessage("The script is ".IPS_GetName($scriptId));
-					
-					$runScript = true;
-					if($updatePresence && $presence==$lastCommonPresence) {
-						$runScript = false;
-						$message = "Old Presence and new Presence is equal. Skipping script";
-					}
-									
-					if($updatePresence && !$presence && $commonPresence) {
-						$runScript = false;
-						$message="Not all users have sent a departure command. Skipping script";
-					}
-					
-					if($runScript) {					
-						if(array_key_exists('delay', $_GET) && is_numeric($_GET['delay'])) {
-							$delay = (int)$_GET['delay'];
-							if($delay>0) {
-								$log->LogMessage("Running script with ".$delay." seconds delay...");
-								$scriptContent = IPS_GetScriptContent($scriptId);
-								$scriptModification =  "//Do not modify this line or the line below\nIPS_SetScriptTimer(\$_IPS['SELF'],0);\n//Do not modify this line or the line above\n";
-								if(strripos($scriptContent, $scriptModification)===false) {
-									$splitPos = strpos($scriptContent, "?>");
-									$scriptPart1 = substr($scriptContent, 0, $splitPos);
-									$scriptPart2 = substr($scriptContent, $splitPos);
-									$scriptContent = $scriptPart1.$scriptModification.$scriptPart2;
-									IPS_SetScriptContent($scriptId, $scriptContent);
-								}
-								IPS_SetScriptTimer($scriptId, $delay);
-							} else {
-								$log->LogMessage("Running script...");
-								IPS_RunScript($scriptId);
-							}
-						} else {
-							$log->LogMessage("Running script...");
-							IPS_RunScript($scriptId);
-						}
-					} else
-						$log->LogMessage($message);				
-				} else 
-					$log->LogMessage("No script is selected for this command");
-				
-				echo "OK";
-			
-			} else
-				$log->LogMessage("Unknown user");
-			
-		} else
-			$log->LogMessage("Invalid or missing \"id\" or \"cmd\" in URL");
-		
 		$this->Unlock("HandleWebData");
     }
 	
-	public function UnregisterUser($Username) {
-		$ident = strtolower(preg_replace("/[^a-zA-Z0-9]+/", "", $Username));
-		$id = IPS_GetObjectIDByIdent($ident, $this->InstanceID);
-		if($id!==false) {
-			$vId = IPS_GetObjectIDByIdent("Presence", $id);
-			if($vId!==false)
-				IPS_DeleteVariable($vId);
-			return IPS_DeleteInstance($id);
-		}
-		
-		$id = IPS_GetInstanceIDByName($Username, $this->InstanceID);
-		if($id!==false) {
-			$vId = IPS_GetObjectIDByName("Presence", $id);
-			if($vId!==false)
-				IPS_DeleteVariable($vId);
-			return IPS_DeleteInstance($id);
-		}
-		
-		return false;
-	}
-
-	public function RegisterUser($Username) {
-		$ident = strtolower(preg_replace("/[^a-zA-Z0-9]+/", "", $Username));
-		$id = IPS_GetObjectIDByIdent($ident, $this->InstanceID);
-		if($id===false) {
-			$id = IPS_GetInstanceIDByName($Username, $this->InstanceID);
-			if($id===false) {
-				$id = IPS_CreateInstance("{C4A1F68D-A34E-4A3A-A5EC-DCBC73532E2C}");
-				IPS_SetName($id,$Username);
-				IPS_SetParent($id,$this->InstanceID);
-				IPS_SetIdent($id, $ident);
-				return true;
-			} else {
-				IPS_SetIdent($id, $ident);
-				
-				return true;
-			}
-		} 
-						
-		return false;
-	}
 	
-	private function GetProfileValueName($Profile, $Value) {
-		$associations = IPS_GetVariableProfile($Profile)['Associations'];
-		
-		$size=sizeof($associations);
-		for($x=0;$x<$size;$x++) {
-		   if($associations[$x]['Value']==$Value) {
-				return $associations[$x]['Name'];
-			}
-		}
-
-		return "Invalid";
-	}
-
     private function RegisterWebHook($Hook, $TargetId) {
 		$id = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
 
@@ -284,25 +95,11 @@ class GeofenceController extends IPSModule {
 		}
     }
 		
-	private function CreateVariable($Parent, $Ident, $Name, $Type, $Profile = "") {
-		$id = @IPS_GetObjectIDByIdent($Ident, $Parent);
-		if($id === false) {
-			$id = IPS_CreateVariable($Type);
-			IPS_SetParent($id, $Parent);
-			IPS_SetName($id, $Name);
-			IPS_SetIdent($id, $Ident);
-			if($Profile != "")
-				IPS_SetVariableCustomProfile($id, $Profile);
-		}
-		
-		return $id;
-	}
-	
 	private function Lock($Ident) {
         $log = new Logging($this->ReadPropertyBoolean("Log"), IPS_Getname($this->InstanceID));
 		for ($x=0;$x<100;$x++)
         {
-            if (IPS_SemaphoreEnter("GEO_".(string)$this->InstanceID.(string)$Ident, 1)){
+            if (IPS_SemaphoreEnter("GH_".(string)$this->InstanceID.(string)$Ident, 1)){
                 return true;
             }
             else {
@@ -315,7 +112,7 @@ class GeofenceController extends IPSModule {
     }
 
     private function Unlock($Ident) {
-        IPS_SemaphoreLeave("GEO_".(string)$this->InstanceID.(string)$Ident);
+        IPS_SemaphoreLeave("GH_".(string)$this->InstanceID.(string)$Ident);
 		$log = new Logging($this->ReadPropertyBoolean("Log"), IPS_Getname($this->InstanceID));
 		$log->LogMessage("The controller is unlocked");
     }
